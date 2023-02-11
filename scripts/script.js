26a984a37b9774363de3162156bc0eec571c3e03
erin-a-z <57600777+erin-a-z@users.noreply.github.com>
2023-02-11 16:49:06 -0500

    fixed the runtime function(async)

--- scripts/script.js
+++ scripts/script.js
@@ -3,30 +3,40 @@ async function fetchData(companyName) {
         method: 'POST',
         headers: {
             'content-type': 'application/json',
-            'X-RapidAPI-Key': '627bcc2db0msh1079ea5154274d6p146d24jsncca5d4921b93',
+            'X-RapidAPI-Key': '65529a08b5msh25845de96de2a6bp1addb5jsn40431c261667',
             'X-RapidAPI-Host': 'you-chat-gpt.p.rapidapi.com'
         },
         body: '{"question":"what is the general consumer opinion on ' + companyName + ' on sustainability?","max_response_time":10}'
     };
 
     const res = await fetch('https://you-chat-gpt.p.rapidapi.com/TextOnly', options)
+    alert('first await')
     const record = await res.json()
-
+    alert(record.answer);
+    alert(typeof (record.answer))
     return record.answer;
 
+
 }
+
+var gptAns;
+
 chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
+    async function (request, sender, sendResponse) {
         console.log(sender.tab ?
-            "from a content script:" + sender.tab.url :
+            "from a content script:" + sender.tab.url + request.greeting :
             "from the extension with message" + request.greeting);
-        if (request.greeting != Null)
+        if (request.greeting != null) {
             sendResponse({ farewell: "goodbye" });
+        }
+        else {
+            alert('null');
+        }
         var brand = request.greeting
-        const gptAns = fetchData(brand)
+        gptAns = await fetchData(brand)
     }
 );
 
-function display(){
+function display() {
     document.getElementById("ans").innerHTML = gptAns;
 }
