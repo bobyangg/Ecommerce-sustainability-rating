479bd55a7084d0d71241ccd250879b1c7256e57e
Bob Yang <87663549+bobyangg@users.noreply.github.com>
2023-02-12 00:13:12 -0500

    Adding colour to stars based on rating received

--- Extension/scripts/script.js
+++ Extension/scripts/script.js
@@ -40,3 +40,19 @@ chrome.runtime.onMessage.addListener(
 function display() {
     document.getElementById("ans").innerHTML = gptAns;
 }
+
+const rating = 0;
+
+
+const star = Math.round(rating / 2);
+if(star == 1){
+    document.getElementById("star1").style.color = gold;
+}else if(star == 2){
+    document.getElementById("star2").style.color = gold;
+}else if(star == 3){
+    document.getElementById("star3").style.color = gold;
+}else if(star == 4){
+    document.getElementById("star4").style.color = gold;
+}else if(star == 5){
+    document.getElementById("star5").style.color = gold;
+}  
